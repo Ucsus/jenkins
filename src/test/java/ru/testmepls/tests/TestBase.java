8c0939b6470e3b23d48b972a8e4f33baf871fbe2
ucsus <ucsusman@gmail.com>
2022-10-10 13:23:35 +0300

    added demoqa test

--- src/test/java/ru/testmepls/tests/TestBase.java
+++ src/test/java/ru/testmepls/tests/TestBase.java
@@ -3,7 +3,10 @@ package ru.testmepls.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import ru.testmepls.helpers.Attach;
+
 public class TestBase {
 
     @BeforeAll
@@ -13,5 +16,12 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
         Configuration.remote="https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
     }
 }
